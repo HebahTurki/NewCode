e8a620d6a2787d2ee7f7a760abeb5bdd23e520f4
Hebah Turki <hebahturki@outlook.com>
2023-03-14 17:06:50 +0300

    Frist commit

---
+++ src/cpit251_hebah/CPIT251_hebah.java
@@ -0,0 +1,16 @@
+package cpit251_hebah;
+
+import java.util.*;
+
+/*
+ * @author hebahturki
+ */
+
+public class CPIT251_hebah 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+
+}
